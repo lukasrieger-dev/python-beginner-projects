51a003536205a112b87e97132458c7e099f91271
lukasrieger-dev <rieger.lks@gmail.com>
2020-04-14 19:03:01 +0200

    add GuessNumberGame

---
+++ GuessNumberGame/src/main.py
@@ -0,0 +1,32 @@
+# Guess Random Number Game
+import random
+
+__author__ = 'codinglukas'
+
+
+MAX_NUMBER = 100
+secret_number = random.randint(0, MAX_NUMBER)
+guess = -1
+rounds = 0
+
+print(f"Can you guess my secret number? Its between 0 and {MAX_NUMBER}")
+
+while True:
+    guess_str = input("Guess my number: ")
+    try:
+        guess = int(guess_str)
+    except ValueError:
+        print(f"Enter a number between 0 and {MAX_NUMBER}")
+        continue
+
+    if guess == secret_number:
+        print("YES, THAT IS MY SECRET NUMBER!")
+        break
+
+    if guess > secret_number:
+        print("NO, MY SECRET NUMBER IS SMALLER.")
+    else:
+        print("NO, MY SECRET NUMBER IS BIGGER.")
+    rounds += 1
+
+print(f">>> YOU FOUND MY SECRET NUMBER AFTER {rounds} GUESSES.")
